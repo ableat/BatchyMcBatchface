f6033f78eae98dcd083b851e989d0181a17f4751
BinaryBeard <sam.bantner@me.com>
2017-04-18 13:32:50 -0400

    Added color to the usage

--- updater.sh
+++ updater.sh
@@ -2,14 +2,24 @@
 
 DYING=0
 
-RED="\033[0;31m"
-CYAN="\033[0;36m"
-WHITE="\033[0;37m"
-ORANGE="\033[0;33m"
-PURPLE="\033[0;35m"
-GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-NC="\033[0m" #No color
+## Constants
+F_BLK="\033[0;30m"
+F_RED="\033[0;31m"
+F_GRN="\033[0;32m"
+F_ORG="\033[0;33m"
+F_BLU="\033[0;34m"
+F_PUR="\033[0;35m"
+F_CYN="\033[0;36m"
+F_LGRY="\033[0;37m"
+F_DGRY="\033[1;30m"
+F_LRED="\033[1;31m"
+F_LGRN="\033[1;32m"
+F_YEL="\033[1;33m"
+F_LBLU="\033[1;34m"
+F_LPUR="\033[1;35m"
+F_LCYN="\033[1;36m"
+F_WHT="\033[0;37m"
+F_CLR="\033[0m"
 
 CONFIG="config.json"
 
@@ -29,16 +39,16 @@ _log() {
 }
 
 _warn() {
-    _log "${YELLOW}WARNING:${NC} ${@}"
+    _log "${F_YEL}WARNING:${F_CLR} ${@}"
 }
 
 _success() {
-    _log "${GREEN}SUCCESS:${NC} ${@}"
+    _log "${F_GRN}SUCCESS:${F_CLR} ${@}"
 }
 
 _die() {
-    _log "${RED}FATAL:${NC} ${@}"
-    _log "${RED}FATAL:${NC} Going down in flames!"
+    _log "${F_RED}FATAL:${F_CLR} ${@}"
+    _log "${F_RED}FATAL:${F_CLR} Going down in flames!"
 
     if [ "${DYING}" -eq 0 ]; then
         DYING=1
@@ -60,23 +70,23 @@ done
 _usage() {
 cat << EOF
 
-Welcome to the GitHub Batch Milestone Updater
+$(echo -e "Welcome to the ${F_CYN}GitHub Batch Milestone Updater${F_CLR}")
 
 This program allows ends users of organizations to
 update specified repositories with new milestones
 
 Please refer to the usage below
 
-usage: ${0##*/} [-h] <command>
+$(echo -e "${F_WHT}usage:${F_CLR} ./${0##*/} ${F_ORG}<command>${F_CLR}")
 
-commands:
-    -h                      display usage
-    -u  username            set github user name (-u binarybeard)
-    -t  token               set github api token (-t "1234567890abcdefg")
-    -o  organization        set github organization (-o Acme)
-    -r  repo1,repo2,etc     set github repos (-r awesome-repo-1,cool_repo_2)
-    -m  milestone           set milestone title (-m "Sprint 7")
-    -d  duration            set milestone duration (-d 7d)
+$(echo -e "${F_YEL}commands:${F_CLR}")
+$(echo -e "    ${F_WHT}-h${F_CLR}                     display usage")
+$(echo -e "    ${F_WHT}-u${F_CLR} ${F_ORG}username${F_CLR}            set github user name (${F_PUR}-u binarybeard${F_CLR})")
+$(echo -e "    ${F_WHT}-t${F_CLR} ${F_ORG}token${F_CLR}               set github api token (${F_PUR}-t \"1234567890abcdefg\"${F_CLR})")
+$(echo -e "    ${F_WHT}-o${F_CLR} ${F_ORG}organization${F_CLR}        set github organization (${F_PUR}-o Acme${F_CLR})")
+$(echo -e "    ${F_WHT}-r${F_CLR} ${F_ORG}repo1,repo2,etc${F_CLR}     set github repos (${F_PUR}-r awesome-repo-1,cool_repo_2${F_CLR})")
+$(echo -e "    ${F_WHT}-m${F_CLR} ${F_ORG}milestone${F_CLR}           set milestone title (${F_PUR}-m \"Sprint 7\"${F_CLR})")
+$(echo -e "    ${F_WHT}-d${F_CLR} ${F_ORG}duration${F_CLR}            set milestone duration (${F_PUR}-d 7d${F_CLR})")
 
 EOF
 }
@@ -150,9 +160,9 @@ gh_up="${GITHUB_USERNAME}:${GITHUB_TOKEN}"
 for gh_repo in "${GITHUB_REPOS[@]}"; do
     repo_response=$(curl -s -o /dev/null -w "%{http_code}" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}" -u "${gh_up}")
     if [ $repo_response -ne "200" ]; then
-        _warn "${PURPLE}${gh_repo}${NC} repository does not exist. Skipping..."
+        _warn "${F_PUR}${gh_repo}${F_CLR} repository does not exist. Skipping..."
     else
-        _log "Updating ${PURPLE}${gh_repo}${NC} repository..."
+        _log "Updating ${F_PUR}${gh_repo}${F_CLR} repository..."
 
         ## Get all Open + Ready/In Progress Issues
         repo_issues_ready=$(curl -s "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues?state=open&labels=ready" -u "${gh_up}" | jq .[].number)
@@ -162,20 +172,20 @@ for gh_repo in "${GITHUB_REPOS[@]}"; do
         milestone_number=$(curl -s -X "POST" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d "{ \"title\": \"${MILESTONE_TITLE}\", \"state\": \"open\", \"description\": \"Welcome to ${MILESTONE_TITLE}. We have all kinds of cool things in store...\", \"due_on\": \"${milestone_due_date}\" }"  | jq .number)
         previous_milestone_number=$((milestone_number-1))
         [[ $previous_milestone_number -lt 0 ]] && previous_milestone_number=0
-        _success "Created ${CYAN}${MILESTONE_TITLE}${NC} milestone"
+        _success "Created ${F_CYN}${MILESTONE_TITLE}${F_CLR} milestone"
 
         ## Add Ready and In Progress Issues to the New Milestone
         for repo_issue in ${repo_issues_to_move[@]}; do
             repo_milestone_number=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues/${repo_issue}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"milestone\": ${milestone_number} }" | jq .milestone.number)
-            [[ $repo_milestone_number -eq $milestone_number ]] && _success "Added ${WHITE}#${repo_issue}${NC} to ${CYAN}${MILESTONE_TITLE}${NC}" || _warn "Did not add ${WHITE}#${repo_issue}${NC} to ${CYAN}${MILESTONE_TITLE}${NC} :("
+            [[ $repo_milestone_number -eq $milestone_number ]] && _success "Added ${F_WHT}#${repo_issue}${F_CLR} to ${F_CYN}${MILESTONE_TITLE}${F_CLR}" || _warn "Did not add ${F_WHT}#${repo_issue}${F_CLR} to ${F_CYN}${MILESTONE_TITLE}${F_CLR} :("
         done
 
         # Close the previous milestone :)
         if [[ $previous_milestone_number -gt 0 ]]; then
             milestone_state=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones/${previous_milestone_number}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"state\": \"closed\" }" | jq -r .state)
-            [[ ${milestone_state} -eq "closed" ]] && _success "${ORANGE}Milestone #${previous_milestone_number}${NC} closed" || _warn "${ORANGE}Milestone #${previous_milestone_number}${NC} was not closed :("
+            [[ ${milestone_state} -eq "closed" ]] && _success "${F_ORG}Milestone #${previous_milestone_number}${F_CLR} closed" || _warn "${F_ORG}Milestone #${previous_milestone_number}${F_CLR} was not closed :("
         fi
 
-        _success "Updated ${PURPLE}${gh_repo}${NC} repository :)"
+        _success "Updated ${F_PUR}${gh_repo}${F_CLR} repository :)"
     fi
 done
