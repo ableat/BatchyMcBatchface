fb7446c850dc7488e2537a869d0255421d159915
BinaryBeard <sam.bantner@me.com>
2017-04-18 11:20:28 -0400

    Added better logging output

--- updater.sh
+++ updater.sh
@@ -3,8 +3,12 @@
 DYING=0
 
 RED="\033[0;31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+ORANGE="\033[0;33m"
+PURPLE="\033[0;35m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
 NC="\033[0m" #No color
 
 CONFIG="config.json"
@@ -84,17 +88,17 @@ cat << EOF > "${CONFIG}"
 EOF
 fi
 
-[[ -z "${GITHUB_USERNAME// }" ]] && GITHUB_USERNAME=$(cat "${CONFIG}" | jq .github.user_name | tr -d '"')
+[[ -z "${GITHUB_USERNAME// }" ]] && GITHUB_USERNAME=$(jq -r .github.user_name "${CONFIG}")
 
-[[ -z "${GITHUB_TOKEN// }" ]] && GITHUB_TOKEN=$(cat "${CONFIG}" | jq .github.api_token | tr -d '"')
+[[ -z "${GITHUB_TOKEN// }" ]] && GITHUB_TOKEN=$(jq -r .github.api_token "${CONFIG}")
 
-[[ -z "${GITHUB_ORG_NAME// }" ]] && GITHUB_ORG_NAME=$(cat "${CONFIG}" | jq .github.organization | tr -d '"')
+[[ -z "${GITHUB_ORG_NAME// }" ]] && GITHUB_ORG_NAME=$(jq -r .github.organization "${CONFIG}")
 
-[[ -z "${MILESTONE_TITLE// }" ]] && MILESTONE_TITLE=$(cat "${CONFIG}" | jq .milestone.title | tr -d '"')
+[[ -z "${MILESTONE_TITLE// }" ]] && MILESTONE_TITLE=$(jq -r .milestone.title "${CONFIG}")
 
-[[ -z "${MILESTONE_DURATION// }" ]] && MILESTONE_DURATION=$(cat "${CONFIG}" | jq .milestone.duration | tr -d '"')
+[[ -z "${MILESTONE_DURATION// }" ]] && MILESTONE_DURATION=$(jq -r .milestone.duration "${CONFIG}")
 
-[[ -z "${GITHUB_REPOS// }" ]] && GITHUB_REPOS=$(cat "${CONFIG}" | jq .github.repos | sed -r 's/(\[|\])//g' | tr -d ',')
+[[ -z "${GITHUB_REPOS// }" ]] && GITHUB_REPOS=( $(jq -r '.github.repos | .[]' "${CONFIG}") )
 
 while getopts ':h u: t: o: r: m: d:' option; do
     case "${option}" in
@@ -129,33 +133,34 @@ gh_up="${GITHUB_USERNAME}:${GITHUB_TOKEN}"
 
 ## List Open and Ready Issues
 for gh_repo in "${GITHUB_REPOS[@]}"; do
-    echo "Updating ${gh_repo} repository..."
+    repo_response=$(curl -s -o /dev/null -w "%{http_code}" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}" -u "${gh_up}")
+    if [ $repo_response -ne "200" ]; then
+        _warn "${PURPLE}${gh_repo}${NC} repository does not exist. Skipping..."
+    else
+        _log "Updating ${PURPLE}${gh_repo}${NC} repository..."
 
-    ## Get all Open + Ready/In Progress Issues
-    repo_issues_ready=$(curl -s "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues?state=open&labels=ready" -u "${gh_up}" | jq .[].number)
-    repo_issues_in_progress=$(curl -s "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues?state=open&labels=in%20progress" -u "${gh_up}" | jq .[].number)
-    repo_issues_to_move=( ${repo_issues_ready[@]} ${repo_issues_in_progress[@]} )
+        ## Get all Open + Ready/In Progress Issues
+        repo_issues_ready=$(curl -s "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues?state=open&labels=ready" -u "${gh_up}" | jq .[].number)
+        repo_issues_in_progress=$(curl -s "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues?state=open&labels=in%20progress" -u "${gh_up}" | jq .[].number)
+        repo_issues_to_move=( ${repo_issues_ready[@]} ${repo_issues_in_progress[@]} )
 
-    ## Create a new Milestone
-    echo "Creating ${MILESTONE_TITLE}..."
-    milestone_number=$(curl -s -X "POST" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d "{ \"title\": \"${MILESTONE_TITLE}\", \"state\": \"open\", \"description\": \"Welcome to ${MILESTONE_TITLE}. We have all kinds of cool things in store...\", \"due_on\": \"${milestone_due_date}\" }"  | jq .number)
-    previous_milestone_number=$((milestone_number-1))
-    [[ $previous_milestone_number -lt 0 ]] && previous_milestone_number=0
-    echo "Finished creating ${MILESTONE_TITLE}"
+        milestone_number=$(curl -s -X "POST" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d "{ \"title\": \"${MILESTONE_TITLE}\", \"state\": \"open\", \"description\": \"Welcome to ${MILESTONE_TITLE}. We have all kinds of cool things in store...\", \"due_on\": \"${milestone_due_date}\" }"  | jq .number)
+        previous_milestone_number=$((milestone_number-1))
+        [[ $previous_milestone_number -lt 0 ]] && previous_milestone_number=0
+        _success "Created ${CYAN}${MILESTONE_TITLE}${NC} milestone"
 
-    ## Add Ready and In Progress Issues to the New Milestone
-    for repo_issue in ${repo_issues_to_move[@]}; do
-        echo "Adding issue #${repo_issue} to ${MILESTONE_TITLE}..."
-        repo_milestone_number=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues/${repo_issue}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"milestone\": ${milestone_number} }" | jq .milestone.number)
-        [[ $repo_milestone_number -eq $milestone_number ]] && echo "Issue #${repo_issue} added to ${MILESTONE_TITLE} sucessfully" || echo "Issue #${repo_issue} was not added to ${MILESTONE_TITLE}. What happened?"
-    done
+        ## Add Ready and In Progress Issues to the New Milestone
+        for repo_issue in ${repo_issues_to_move[@]}; do
+            repo_milestone_number=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/issues/${repo_issue}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"milestone\": ${milestone_number} }" | jq .milestone.number)
+            [[ $repo_milestone_number -eq $milestone_number ]] && _success "Added ${WHITE}#${repo_issue}${NC} to ${CYAN}${MILESTONE_TITLE}${NC}" || _warn "Did not add ${WHITE}#${repo_issue}${NC} to ${CYAN}${MILESTONE_TITLE}${NC} :("
+        done
 
-    # Close the previous milestone :)
-    if [[ $previous_milestone_number -gt 0 ]]; then
-        echo "Closing milestone #${previous_milestone_number}..."
-        milestone_state=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones/${previous_milestone_number}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"state\": \"closed\" }" | jq -r .state)
-        [[ ${milestone_state} -eq "closed" ]] && echo "Milestone #${previous_milestone_number} closed sucessfully" || echo "Milestone #${previous_milestone_number} was not closed :("
+        # Close the previous milestone :)
+        if [[ $previous_milestone_number -gt 0 ]]; then
+            milestone_state=$(curl -s -X "PATCH" "${gh_repos_url}/${GITHUB_ORG_NAME}/${gh_repo}/milestones/${previous_milestone_number}" -H "Content-Type: application/json; charset=utf-8" -u ${gh_up} -d $"{ \"state\": \"closed\" }" | jq -r .state)
+            [[ ${milestone_state} -eq "closed" ]] && _success "${ORANGE}Milestone #${previous_milestone_number}${NC} closed" || _warn "${ORANGE}Milestone #${previous_milestone_number}${NC} was not closed :("
+        fi
+
+        _success "Updated ${PURPLE}${gh_repo}${NC} repository :)"
     fi
-
-    echo "Completed updating ${gh_repo} repository :)\n"
 done
